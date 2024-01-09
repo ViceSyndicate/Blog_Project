65ebe81b8d6cf43913e90ba0d7d739d9d5e19e41
Victor S <victor.skarerverket@gmail.com>
2024-01-09 11:09:57 +0100

    Added DataLibrary for the backend

---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLibrary.DataAccess
+{
+    internal class SqlDataAccess
+    {
+    }
+}
---
+++ DataLibrary/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Model
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}

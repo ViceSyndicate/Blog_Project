9f3f16f10bc8fd7a9440efbd7bc790283ca7910c
Victor S <victor.skarerverket@gmail.com>
2024-03-17 01:32:39 +0100

    Added explanation comments to PaginatedList.cs

--- Blog_Project/PaginatedList.cs
+++ Blog_Project/PaginatedList.cs
@@ -14,9 +14,9 @@ namespace Blog_Project
 
             this.AddRange(items);
         }
-
+        // True if we're not at PageIndex 1. 
         public bool HasPreviousPage => PageIndex > 1;
-
+        // True if current Page is less than the total nr of pages.
         public bool HasNextPage => PageIndex < TotalPages;
 
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
@@ -28,6 +28,9 @@ namespace Blog_Project
         public static async Task<PaginatedList<T>> Create(List<T> source, int pageIndex, int pageSize)
         {
             var count = source.Count;
+            // source.Skip((pageIndex - 1) * pageSize) should get the nr of page +1 * 5
+            // to skip all the previous items in the list 
+            // .Take(pageSize).ToList(); Takes the coming 5 items and turns them to a list. 
             var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
